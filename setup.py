e77d9d4187806dcd4be5bb75a75871a144e1889c
Piotr Niewinski <me@piotrniewinski.com>
2023-03-08 18:11:16 +0100

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements():
 
 setup(
     name=               'torchness',
-    version=            'v0.1.2',
+    version=            'v1.0.0',
     url=                'https://github.com/piteren/torchness.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
