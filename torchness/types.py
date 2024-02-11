cd76610fb1a029298ef81ed381776d66a02837de
Piotr Niewinski <me@piotrniewinski.com>
2024-02-11 23:27:36 +0100

    upd types

--- torchness/types.py
+++ torchness/types.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from typing import Optional, Callable, Dict, Union, Any, Iterable
+from typing import Optional, Callable, Dict, Union, Any, Sequence
 
 ACT = Optional[type(torch.nn.Module)]       # activation type
 INI = Optional[Callable]                    # initializer type
@@ -9,4 +9,4 @@ TNS = torch.Tensor                          # Tensor
 DTNS = Dict[str, Union[TNS,Any]]            # dict {str: Tensor or Any}
 
 NUM = Union[int, float, np.ndarray, TNS]    # extends pypaq NUM with Tensor
-NPL = Union[Iterable[NUM], np.ndarray, TNS] # extends pypaq NPL with Tensor
+NPL = Union[Sequence[NUM], np.ndarray, TNS] # extends pypaq NPL with Tensor
