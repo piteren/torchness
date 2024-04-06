3840043ef9bd6457044666f757fb87a51f5c0396
ppp <me@piotrniewinski.com>
2024-04-06 20:51:41 +0200

    Update version to v1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'torchness',
-    version=            'v1.3.1',
+    version=            'v1.3.2',
     url=                'https://github.com/piteren/torchness.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
