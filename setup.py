5b06fcf7c3419cdd3ac8b1c4d19b281c1434a177
pionie <pioniewinski@gmail.com>
2024-08-06 21:44:58 +0200

    Update version to v1.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'torchness',
-    version=            'v1.4.4',
+    version=            'v1.4.5',
     url=                'https://github.com/piteren/torchness.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
